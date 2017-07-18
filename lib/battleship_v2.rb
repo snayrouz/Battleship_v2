bdfac892967c607a6f5ca2840d031195367a1b44
snayrouz <samuel.nayrouz@gmail.com>
2017-07-18 16:44:01 -0600

    Move messages from v1

--- lib/battleship_v2.rb
+++ lib/battleship_v2.rb
@@ -1,2 +1,19 @@
 # don't make this a class
 # make it a true runner file
+include Messages
+
+puts Welcome
+
+while true
+
+
+
+
+  if answer == "p" || answer == "play"
+    @game
+  elsif answer == "q" || answer == "quit"
+    exit
+  end
+
+
+end
---
+++ lib/messages.rb
@@ -0,0 +1,78 @@
+require 'colorize'
+module Messages
+
+  def welcome
+    prints "Welcome to BATTLESHIP".colorize(:cyan, :bright)
+  end
+
+  def options
+    puts "Would you like to (p)lay, read the (i)nstructions, or (q)uit?".colorize(:cyan)
+    answer = gets.chomp.downcase
+    if answer == "p" || answer == "play"
+      @game
+    elsif answer == "i" || answer == "instructions".
+      instructions
+      options
+    elsif answer == "q" || answer == "quit"
+      exit
+    else
+      puts "That's not an option. But no problem, let's get back to the option menu.".colorize(:red)
+      options
+    end
+  end
+
+  def instructions
+    puts "Try and sink all your enemy's ships before they sink all of yours!"
+    puts "You won't be able to see the computer's board so you'll have to try and guess where their ships are."
+    puts "When all of the squares that your ships occupy are hit, the ship will be sunk."
+    puts "The first player who sinks all of their opponents ships wins, and the game ends."
+    puts "Would you like to (p)lay or (q)uit game?".colorize(:cyan, :bright)
+    answer = get.chomp.downcase
+    
+  end
+
+  def cpu_intro
+    puts "I have laid out my ships on the grid.
+    \nYou now need to layout your two ships.
+    \nThe first is two units long and the\nsecond is three units long.
+    \nThe grid has A1 at the top left and D4 at the bottom right.
+    \nEnter the coordinates for the two unit ship: ".colorize(:magenta)
+  end
+
+  def player_miss
+    "You fired at #{cell} and missed!".colorize(:red, :bright)
+  end
+
+  def comp_miss
+    output.puts "Your enemy fired at #{cell} missed!".colorize(:red, :bright)
+  end
+
+  def player_invailid_placement
+    "Invalid entry for ship placement, try again: ".colorize(:red, :bright)
+  end
+
+  def player_invalid_shot
+    "Invalid shot, try again: ".colorize(:red, :bright)
+  end
+
+  def invalid_length
+    "Your ship is either too long or too short. Try again: ".colorize(:red, :bright)
+  end
+
+  def direct_hit(cell)
+    "Direct hit at #{cell}!".colorize(:yellow)
+  end
+
+  def ask_for_third_ship
+    "Now enter the placement coordinates for your long ship: ".colorize(:green)
+  end
+
+  def player_turn
+    "It's your turn to fire! Enter your selection: ".colorize(:green)
+  end
+
+  def player_win
+    "You won! Congrats, now go on and do a little dance!".colorize(:magenta)
+  end
+
+end
