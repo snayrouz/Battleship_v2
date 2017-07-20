136a6851802c60de139263dd6fc4bb80d719cb05
snayrouz <samuel.nayrouz@gmail.com>
2017-07-20 07:35:59 -0600

    Player test

---
+++ test/player_test.rb
@@ -0,0 +1,44 @@
+require "minitest/autorun"
+require "minitest/pride"
+require_relative '../lib/player'
+require_relative '../lib/computer_player'
+require_relative '../lib/grid'
+require_relative '../lib/ship'
+
+class PlayerTest < Minitest::Test
+
+  def test_player_can_place_ships
+    player = Player.new
+
+    player.place_ship(['A1', 'A2'])
+    board = player.player_grid.board
+
+    assert_instance_of Ship, board['C', '1'].ship
+    assert_instance_of Ship, board['C', '2'].ship
+  end
+
+  def test_set_ships_gives_correct_results
+    expected = ['small', 'large']
+    actual = player.set_ships(2).keys
+
+    assert_equal expected, actual
+    assert_instance_of Ship, player.set_ships(2)['small']
+  end
+
+  def test_player_can_shoot_at_computer
+    player = Player.new
+    small = Ship.new
+    computer_grid.place_ship_at('B2', small)
+    computer_grid.place_ship_at('B4', small)
+    expected = [[' ', ' ', ' ', ' '],
+                [' ', ' ', 'H', ' '],
+                [' ', ' ', 'M', ' '],
+                [' ', ' ', ' ', ' ']]
+
+    player.shoot_at('B3')
+    player.shoot_at('C3')
+
+    assert_equal expected, cpu_grid.board_layout
+  end
+
+end
