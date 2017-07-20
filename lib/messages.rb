9c7b97132e9551c7f1243930ab65ba228dafd752
snayrouz <samuel.nayrouz@gmail.com>
2017-07-19 20:31:07 -0600

    Delete colorize

--- lib/messages.rb
+++ lib/messages.rb
@@ -1,13 +1,11 @@
-# require 'colorize'
-
 module Messages
 
   def welcome
-    prints "Welcome to BATTLESHIP".colorize(:cyan, :bright)
+    prints "Welcome to BATTLESHIP"
   end
 
   def options
-    puts "Would you like to (p)lay, read the (i)nstructions, or (q)uit?".colorize(:cyan)
+    puts "Would you like to (p)lay, read the (i)nstructions, or (q)uit?"
   end
 
   def instructions
@@ -15,7 +13,7 @@ module Messages
     puts "You won't be able to see the computer's board so you'll have to try and guess where their ships are."
     puts "When all of the squares that your ships occupy are hit, the ship will be sunk."
     puts "The first player who sinks all of their opponents ships wins, and the game ends."
-    puts "Would you like to (p)lay or (q)uit game?".colorize(:cyan, :bright)
+    puts "Would you like to (p)lay or (q)uit game?"
   end
 
   def comp_intro
@@ -23,71 +21,71 @@ module Messages
     \nYou now need to layout your two ships.
     \nThe first is two units long and the\nsecond is three units long.
     \nThe grid has A1 at the top left and D4 at the bottom right.
-    \nEnter the coordinates for the two unit ship: ".colorize(:magenta)
+    \nEnter the coordinates for the two unit ship: "
   end
 
   def sunkage_comp
-    "The machines have won! Just but defintely sunk your ship".colorize(:red)
+    puts "The machines have won! Just but defintely sunk your ship"
   end
 
   def sunkage_player
-    "Nice job! You sunk the computer's ship!".colorize(:green)
+    puts "Nice job! You sunk the computer's ship!"
   end
 
-  def player_miss
-    "You fired at #{cell} and missed!".colorize(:red, :bright)
-  end
+  # def player_miss
+  #   "You fired at #{cell} and missed!"
+  # end
 
   def order
-    "That order structure is not correct".colorize(:red, :bright)
+    puts "That order structure is not correct"
   end
 
   def cell_full
-    "That space is already occupied".colorize(:red, :bright)
+    puts "That space is already occupied"
   end
 
   def comp_miss
-    "Your enemy fired at #{cell} missed!".colorize(:red, :bright)
+    puts "Your enemy fired at #{cell} missed!"
   end
 
   def invalid_coordinates_submit
-    "That's not a valid format. Try something like (A1 B2)".colorize(:red, :bright)
+    puts "That's not a valid format. Try something like (A1 B2)"
   end
 
   def player_invalid_placement
-    "Invalid entry for ship placement, try again: ".colorize(:red, :bright)
+    puts "Invalid entry for ship placement, try again: "
   end
 
   def player_invalid_shot
-    "Invalid shot, try again: ".colorize(:red, :bright)
+    puts "Invalid shot, try again: "
   end
 
   def invalid_length
-    "Your ship is either too long or too short. Try again: ".colorize(:red, :bright)
+    puts "Your ship is either too long or too short. Try again: "
   end
 
-  def direct_hit(cell)
-    "Direct hit at #{cell}!".colorize(:yellow)
-  end
+  # def direct_hit(cell)
+  #   "Direct hit at #{cell}!"
+  # end
 
   def ask_for_third_ship
-    "Now enter the placement coordinates for your long ship: ".colorize(:green)
+    puts "Now enter the placement coordinates for your long ship: "
   end
 
   def player_turn
-    "It's your turn to fire! Enter your selection: ".colorize(:green)
+    puts "It's your turn to fire! Enter your selection: "
   end
 
   def player_win
-    "You won! Congrats, now go on and do a little dance!".colorize(:magenta)
+    puts "You won! Congrats, now go on and do a little dance!"
   end
 
   def computer_win
-    "You lose! This sure is a hit or miss kind of game ;)".colorize(:red, :bright)
+    puts "You lose! This sure is a hit or miss kind of game ;)"
   end
 
   def invalid_selection
-    "That wasn't even close to being an option. Try again pal: ".colorize(:red, :bright)
+    puts "That wasn't even close to being an option. Try again pal: "
   end
 
 end
