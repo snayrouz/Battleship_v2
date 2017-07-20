aeca45c32e09673bb2fdadb5fcc58ba76be9af21
snayrouz <samuel.nayrouz@gmail.com>
2017-07-20 08:12:33 -0600

    Computer test progression

--- test/computer_test.rb
+++ test/computer_test.rb
@@ -9,6 +9,7 @@ class ComputerTest < Minitest::Test
   def setup
     @small = 2
     @large = 3
+    @computer = Computer.new(computer_grid, player_grid)
   end
 
   def test_can_change_ship_orientation
@@ -20,5 +21,20 @@ class ComputerTest < Minitest::Test
     assert expected.include?(ship.orientation)
   end
 
+  def computer_can_place_ship_not_overlapping
+    computer.place_ships
+    small_coordinates = computer.ships['small'].coordinates
+    large_coordinates = computer.ships['large'].coordinates
 
+    assert (small_coordinates && large_coordinates).empty?
+  end
+
+  def test_computer_knows_possible_ships
+    expected = { 'small' => 2, 'large' => 3}
+
+    assert_instance_of Computer, computer
+    assert_equal expected, computer.arsenal_size
+  end
+
+  #test class unfinished
 end
