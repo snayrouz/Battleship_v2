ec247facecfb277ca726cbc7f5175bea1feb9c9c
snayrouz <samuel.nayrouz@gmail.com>
2017-07-20 07:28:11 -0600

    Game test

--- test/game_test.rb
+++ test/game_test.rb
@@ -2,6 +2,7 @@
 require 'minitest/autorun'
 require 'minitest/pride'
 require './lib/game'
+require './lib/messages'
 
 
 class GameTest < Minitest::Test
@@ -37,16 +38,33 @@ class GameTest < Minitest::Test
 
   def test_coordinate_returns_error
     game = Game.new
-    occupied = ['A1', 'A2']
     invalid = ['A1', 'B3']
     short =["A1"]
     long = ['A1', 'A5']
 
     game.setup('D4')
 
-    assert_equal 
-
+    assert_equal
+    assert_equal invalid, Messages.player_invalid_placement
+    assert_equal short, Messages.invalid_length
+    assert_equal long, Messages.invalid_length
   end
 
+
+  def test_computer_can_place_ships
+    skip #unsure how to test random
+    game = Game.new
+
+    game.setup('D4')
+
+    assert
+  end
+
+  def test_who_can_win
+    game = Game.new
+
+    game.computer_grid.occupied_cells.clear
+    assert_instance_of Player, game.winner  
+  end
 
 end
