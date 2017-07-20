6db99c58aac67d6329080520de79d689e8ba9f98
snayrouz <samuel.nayrouz@gmail.com>
2017-07-20 07:59:47 -0600

    Cell test

--- test/cell_test.rb
+++ test/cell_test.rb
@@ -14,23 +14,28 @@ class CellTest < Minitest::Test
 
   def test_cell_empty_by_default
     assert_instance_of Cell, cell
+
     refute.cell.occupied
   end
 
   def test_cell_can_become_occupied
     cell.place_ship(ship)
+
     assert cell.occupied
   end
 
   def test_knows_miss
+    cell.shoot_at('A1')
 
-
+    assert_equal 'M', cell.status
   end
 
 
   def test_knows_hit
+    cell.place_ship(ship)
+    cell.shoot_at('A1')
 
-
+    assert_equal 'H', cell.status
   end
 
 
